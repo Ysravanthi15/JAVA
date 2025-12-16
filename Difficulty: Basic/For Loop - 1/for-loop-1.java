ff2edaa2948266a6bc0a27e4f10050228b3a1b85
Sravanthi Yedla <167872100+Ysravanthi15@users.noreply.github.com>
2025-12-16 20:06:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/For Loop - 1/for-loop-1.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static void utility(int n) {
+        // code here
+        
+        for(int i=1;i<=10;i++){
+            int mul=n*i;
+            System.out.print(mul+" ");
+        }
+    }
+}
