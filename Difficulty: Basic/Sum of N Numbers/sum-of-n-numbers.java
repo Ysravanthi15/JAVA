a8995bbe0d40728e9b89cd60864dc8a6533826da
Sravanthi Yedla <167872100+Ysravanthi15@users.noreply.github.com>
2025-12-27 21:15:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Sum of N Numbers/sum-of-n-numbers.java
+++ Difficulty: Basic/Sum of N Numbers/sum-of-n-numbers.java
@@ -1,10 +1,10 @@
 class Solution {
     public static int nSum(int n) {
         // code here
-        int ans = 0;
+        int count=0;
         for(int i=1;i<=n;i++){
-            ans+=i;
+            count+=i;
         }
-        return ans;
+        return count;
     }
 }
